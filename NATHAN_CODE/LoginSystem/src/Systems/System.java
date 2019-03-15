b103d16aa11dc932daad2f7305d1b6467dd5accd
nw1510 <47594706+nw1510@users.noreply.github.com>
2019-03-15 14:41:24 +0000

    Add files via upload
    Designed some UI components using java swing

---
+++ NATHAN_CODE/LoginSystem/src/Systems/System.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Systems;
+
+/**
+ *
+ * @author jamie2
+ */
+public class System {
+    public static void main(String args[]) {
+    
+        
+     /**
+     * @param args the command line arguments
+     */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new LoginUI().setVisible(true);
+            }
+        });
+    }
+}
