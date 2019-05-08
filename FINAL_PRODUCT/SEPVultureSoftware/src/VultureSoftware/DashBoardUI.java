f0fae757cacca770edacc06806877f2f552efac9
JamieShiels <47561193+JamieShiels@users.noreply.github.com>
2019-05-08 09:14:29 +0100

    fixed buttons enabled/disabled

--- FINAL_PRODUCT/SEPVultureSoftware/src/VultureSoftware/DashBoardUI.java
+++ FINAL_PRODUCT/SEPVultureSoftware/src/VultureSoftware/DashBoardUI.java
@@ -204,10 +204,7 @@ public class DashBoardUI extends javax.swing.JFrame {
         {
             adminButton.setEnabled(false);
             jobDelayButton.setEnabled(false);
-            taskAllocatorButton.setEnabled(true);
-            //orderPartsButton.setEnabled(false);
-            taskAllocatorButton.setEnabled(false);
-
+            graphButton.setEnabled(false);
             //needs current jobs
             
         }
