e6804fea17c4b81833b942be672ea9c263e3520d
JamieShiels <47561193+JamieShiels@users.noreply.github.com>
2019-05-10 14:32:43 +0100

    deleted job allocator test

--- FINAL_PRODUCT/SEPVultureSoftware/test/VultureSoftware/VultureSoftwareSuite.java
+++ FINAL_PRODUCT/SEPVultureSoftware/test/VultureSoftware/VultureSoftwareSuite.java
@@ -15,7 +15,7 @@ import org.junit.runners.Suite;
  * @author jamie
  */
 @RunWith(Suite.class)
-@Suite.SuiteClasses({VultureSoftware.JobAllocatorUITest.class, VultureSoftware.DeleteUserUITest.class, VultureSoftware.JobDelayUITest.class, VultureSoftware.TaskAllocatorUITest.class, VultureSoftware.MotorEditUITest.class, VultureSoftware.DriverTest.class, VultureSoftware.FinalInspectionUITest.class, VultureSoftware.AllocateATaskUITest.class, VultureSoftware.ViewGraphUITest.class, VultureSoftware.AdminUITest.class, VultureSoftware.DailyTaskUITest.class, VultureSoftware.SearchEditUserUITest.class, VultureSoftware.CurrentJobsUITest.class, VultureSoftware.LoginDriverTest.class, VultureSoftware.MotorAddUITest.class, VultureSoftware.DashBoardUITest.class, VultureSoftware.TaskListUITest.class, VultureSoftware.LoginUITest.class, VultureSoftware.NewUserUITest.class, VultureSoftware.EditUserUITest.class})
+@Suite.SuiteClasses({VultureSoftware.DeleteUserUITest.class, VultureSoftware.JobDelayUITest.class, VultureSoftware.TaskAllocatorUITest.class, VultureSoftware.MotorEditUITest.class, VultureSoftware.DriverTest.class, VultureSoftware.FinalInspectionUITest.class, VultureSoftware.AllocateATaskUITest.class, VultureSoftware.ViewGraphUITest.class, VultureSoftware.AdminUITest.class, VultureSoftware.DailyTaskUITest.class, VultureSoftware.SearchEditUserUITest.class, VultureSoftware.CurrentJobsUITest.class, VultureSoftware.LoginDriverTest.class, VultureSoftware.MotorAddUITest.class, VultureSoftware.DashBoardUITest.class, VultureSoftware.TaskListUITest.class, VultureSoftware.LoginUITest.class, VultureSoftware.NewUserUITest.class, VultureSoftware.EditUserUITest.class})
 public class VultureSoftwareSuite {
 
     @BeforeClass
