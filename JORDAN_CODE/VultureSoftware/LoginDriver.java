7350dd3204d17428b48a7c953bd65bf3d05dd3e1
Jordio99 <47435037+Jordio99@users.noreply.github.com>
2019-05-13 16:13:22 +0100

    temp stuff working off this for now

---
+++ JORDAN_CODE/VultureSoftware/LoginDriver.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package VultureSoftware;
+
+import databaseui.*;
+/**
+ * 
+ * 
+ * *
+ * @author w17014747
+ */
+public class LoginDriver {
+ public static  void main(String[] args)
+ {
+     new DataBaseConnection();
+ }
+}
